5a3bab0d81a4101c66a3f27dece0af35c061f595
judezhan <judezhan@gmail.com>
2017-08-29 00:11:52 +0800

    Update lambda.cc

--- lambda.cc
+++ lambda.cc
@@ -4,9 +4,20 @@ int main() {
     int a = 10;
     std::cout << "a = " << a << std::endl;
     auto f = [a]() -> int{return a;};
+    auto f2 = [a]() mutable -> int{return ++a;};
+    auto f3 = [](int x) -> int {
+        if (x < 0)
+            return -x;
+        else
+            return x;
+    };
     a = 0;
     auto x = f();
+    auto x2 = f2();
+    auto x3 = f3(-1);
     std::cout << "a = " << a << std::endl;
     std::cout << "x = " << x << std::endl;
+    std::cout << "x2 = " << x2 << std::endl;
+    std::cout << "x3 = " << x3 << std::endl;
     return 0;
 }
---
+++ transform.cc
@@ -0,0 +1,36 @@
+// transform algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::transform
+#include <vector>       // std::vector
+
+int main () {
+  std::vector<int> foo;
+  std::vector<int> bar;
+
+  // set some values:
+  for (int i=1; i<6; i++)
+    foo.push_back (i*10);                         // foo: 10 20 30 40 50
+
+  bar.resize(foo.size());                         // allocate space
+
+  std::transform (foo.begin(), foo.end(), bar.begin(), [](int i) -> int{
+    if (i < 0)
+      return -i;
+    else if (i == 0) {
+      return i;
+    } else {
+      return i;
+    }
+  });
+
+  std::cout << "foo contains:";
+  for (std::vector<int>::iterator it=foo.begin(); it!=foo.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+  std::cout << "bar contains:";
+  for (std::vector<int>::iterator it=bar.begin(); it!=bar.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
+}
